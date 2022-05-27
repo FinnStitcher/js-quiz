a4d3a512e71a72f0d4eefeac130232d39a5274e8
Finn Stitcher <foundfootage@protonmail.com>
2022-05-27 15:04:47 -0700

    html skeleton

---
+++ assets/js/script.js
@@ -0,0 +1,42 @@
+var questions = [
+    {
+        question: "Commonly used data types do NOT include:",
+        answer1: "strings",
+        answer2: "booleans",
+        answer3: "alerts",
+        answer4: "numbers",
+        correct: "a3"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with:",
+        answer1: "quotes",
+        answer2: "curly brackets",
+        answer3: "parentheses",
+        answer4: "square brackets",
+        correct: "a3"
+    },
+    {
+        question: "Arrays in JavaScript can be used to store:",
+        answer1: "numbers and strings",
+        answer2: "other arrays",
+        answer3: "booleans",
+        answer4: "all of the above",
+        correct: "a4"
+    },
+    {
+        question: "String values must be enclosed within _____ when being assigned to variables.",
+        answer1: "commas",
+        answer2: "curly brackets",
+        answer3: "quotes",
+        answer4: "parentheses",
+        correct: "a3"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content is:",
+        answer1: "JavaScript",
+        answer2: "terminal/bash",
+        answer3: "for loops",
+        answer4: "console.log",
+        correct: "a4"
+    },
+];
